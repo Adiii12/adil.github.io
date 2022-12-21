9c135c8ba9e2d725e9ed42bf16d6dc0348fbff4d
Muhammad Adil Khan <67166105+Adiii12@users.noreply.github.com>
2022-12-21 20:55:35 +0500

    new portfoilio website
    Polakaaron portfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    const menu = document.getElementById("menuContent");
+    menu.style.right = "0px";
+}
+function closeMenu(){
+    const menu = document.getElementById("menuContent");
+    menu.style.right = "-100%";
+}
